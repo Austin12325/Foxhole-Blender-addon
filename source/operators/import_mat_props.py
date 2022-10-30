59e1210dd80a84c9065596d92132c1dc8036e047
Austin <Ajlimo1980@gmail.com>
2022-10-30 15:20:33 -0500

    Update import_mat_props.py
    Fixed screen area
    Added last imported text file open in text editor

--- source/operators/import_mat_props.py
+++ source/operators/import_mat_props.py
@@ -4,12 +4,12 @@ from glob import glob
     #https://stackoverflow.com/questions/14798220/how-can-i-search-sub-folders-using-glob-glob-module
 dirpath = bpy.data.scenes["Scene"].sna_umodel_exportpath
 matname = bpy.context.active_object.material_slots[0].name+'.props.txt'
-new_area = bpy.context.screen.areas[-1]
+blendpath = bpy.data.filepath
 
 
-if bpy.context.screen.show_fullscreen == False:
+if bpy.context.screen.show_fullscreen == False:                                 # Throw report if the user is in fullscreen mode (can't split window if in fullscreen)
     #for path in glob(dirpath, recursive=True):
-    for matslots in bpy.context.object.material_slots:
+    for matslots in bpy.context.object.material_slots:                          # Get the name of every material on selected object, search for it in exported file
         for file in glob(dirpath+'/**/*'+matslots.name+'.props.txt',recursive=True):
             bpy.ops.text.open(filepath=file)
             print(file)
@@ -20,14 +20,19 @@ else:
 
 
 
-
-if bpy.context.window_manager.windows[-1].screen.areas[-1].type == "TEXT_EDITOR":
-    pass
+if bpy.context.screen.show_fullscreen == False:
+    if bpy.context.screen.areas[0].type == "TEXT_EDITOR":
+        pass
+    else:                                                                       # Split window and newly created text editor to view the last imported file.
+        bpy.ops.screen.area_split(direction='VERTICAL', factor=0.75)
+        bpy.context.screen.areas[-1].type = "TEXT_EDITOR"
+        
+        text = bpy.data.texts[-1]
+        for area in bpy.context.screen.areas:
+            if area.type == 'TEXT_EDITOR':
+                area.spaces[0].text = text
 else: 
-    bpy.ops.screen.area_split(direction='VERTICAL', factor=0.75)
-    bpy.context.window_manager.windows[-1].screen.areas[-1].type = "TEXT_EDITOR"
-
-
+    self.report({'INFO'}, 'Please leave fullscreen mode (shift spacebar)')
 
 bpy.ops.wm.save_as_mainfile(filepath=bpy.data.filepath)
 print('files imported')
