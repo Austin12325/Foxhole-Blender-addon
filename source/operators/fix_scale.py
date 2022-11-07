52d08991235c7f848a428b2aff5c048ec762b88f
Austin <Ajlimo1980@gmail.com>
2022-11-07 02:03:45 -0600

    Scale object
    Remove armature scaling in favor of scaling selected objects

--- source/operators/fix_scale.py
+++ source/operators/fix_scale.py
@@ -1,6 +1,4 @@
 import bpy
-    bpy.ops.object.select_all(action='DESELECT')
-    bpy.data.objects['Armature'].select_set(state=True)
-    bpy.ops.transform.resize(value=(100, 100, 100), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+    bpy.ops.transform.resize(value=(100, 100, 100))
     bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
-    bpy.ops.transform.resize(value=(0.01, 0.01, 0.01), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+    bpy.ops.transform.resize(value=(0.01, 0.01, 0.01))
